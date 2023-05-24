84c0cc33d59676b812dcc6bbf8082301ecce0561
Lidier López <llopezraccioppe@danielcastelao.org>
2023-05-24 10:33:31 +0200

    Comite inicial

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+        public static void main(String[] args) {
+                // Este sera el proyecto de Lidier Máximo López Raccioppe y Jon luc Marcos.
+                // El proyecto sera hacer un programa parecido a trivago pero en java.
+        }
+}
