ccad0de978a10c81ca045d27296c36d772fe7968
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-02 09:27:20 +0200

    agrego constructor de usuario y su javadoc

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -5,4 +5,24 @@ public class Usuario {
      * Nombre del usuario e identificador
      */
     private String nombreUsuario;
+    /**
+     * Clave al momento de loggearse
+     */
+    private String contraseña;
+
+    /**
+     * Constructor vacío de Usuario
+     */
+    public Usuario() {
+    }
+
+    /**
+     * Constructor vacío de Usuario
+     * @param nombreUsuario Nombre del usuario e identificador
+     * @param contraseña Clave al momento de loggearse
+     */
+    public Usuario(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
 }
