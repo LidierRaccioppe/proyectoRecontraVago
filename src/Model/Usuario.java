222835e597289d887028822107432dd25c1b2b72
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-02 09:34:43 +0200

    agrego getters y setters de usuario y su javadoc

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -25,4 +25,36 @@ public class Usuario {
         this.nombreUsuario = nombreUsuario;
         this.contraseña = contraseña;
     }
+
+    /**
+     * Obtiene el nombre del usuario
+     * @return nombre que el usuario tiene
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * establece el nombre del usuario
+     * @param nombreUsuario el string que queremos que sea el nuevo nombre del usuario
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    /**
+     * Obtiene la contraseña del usuario
+     * @return contraseña que el usuario tiene
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * establece la contraseña del usuario
+     * @param contraseña el string que queremos que sea la nueva contraseña del usuario
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
