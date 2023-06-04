8cb17dec2931880a6b99c613ecccf7f5b6e7f96a
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 21:53:40 +0200

    agrego los constructores a Hotel y agrego su java doc

--- src/Model/Hotel.java
+++ src/Model/Hotel.java
@@ -9,4 +9,44 @@ public class Hotel extends Edificio{
      * La calidad de los servicios que da el edificio
      */
     private float calidadServicios;
+
+    /**
+     *  Constructor con valores de Hotel y valores heredados de Edificio
+     * @param nombreRecidencia nombre e identificador del Edificio
+     * @param calle calle en la que se encuentra el Edificio
+     * @param numEdificioCalle número correspondiente del Edificio en la calle que se encuentra
+     * @param precioHabitacion el precio de las habitaciones del hotel
+     * @param estrellasUsuario la valoracion que le dieron los usuarios al hotel
+     * @param numHabitacionesTotales el numero total de habitaciones que tiene el hotel
+     * @param numPisos Número de pisos que tiene el edificio
+     * @param calidadServicios La calidad de los servicios que da el edificio
+     */
+    public Hotel(String nombreRecidencia, String calle, int numEdificioCalle, float precioHabitacion, int estrellasUsuario, int numHabitacionesTotales, int numPisos, float calidadServicios) {
+        super(nombreRecidencia, calle, numEdificioCalle, precioHabitacion, estrellasUsuario, numHabitacionesTotales);
+        this.numPisos = numPisos;
+        this.calidadServicios = calidadServicios;
+    }
+
+    /**
+     * Contstructor de solo valores de Hotel
+     * @param numPisos Número de pisos que tiene el edificio
+     * @param calidadServicios La calidad de los servicios que da el edificio
+     */
+    public Hotel(int numPisos, float calidadServicios) {
+        this.numPisos = numPisos;
+        this.calidadServicios = calidadServicios;
+    }
+
+    /**
+     * Constructor sin valores de Hotel y con valores de Edificio
+     * @param nombreRecidencia nombre e identificador del Edificio
+     * @param calle calle en la que se encuentra el Edificio
+     * @param numEdificioCalle número correspondiente del Edificio en la calle que se encuentra
+     * @param precioHabitacion el precio de las habitaciones del hotel
+     * @param estrellasUsuario la valoracion que le dieron los usuarios al hotel
+     * @param numHabitacionesTotales el numero total de habitaciones que tiene el hotel
+     */
+    public Hotel(String nombreRecidencia, String calle, int numEdificioCalle, float precioHabitacion, int estrellasUsuario, int numHabitacionesTotales) {
+        super(nombreRecidencia, calle, numEdificioCalle, precioHabitacion, estrellasUsuario, numHabitacionesTotales);
+    }
 }
