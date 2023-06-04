58756ecaab929513434d96c115590a9f99b215b4
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 20:13:38 +0200

    agrego la variable numEdificioCalle y su java doc

--- src/Model/Edificio.java
+++ src/Model/Edificio.java
@@ -5,4 +5,12 @@ public class Edificio {
      * Nombre del edificio e identificador
      */
     private String nombreRecidencia;
+    /**
+     * Calle del edificio
+     */
+    private String calle;
+    /**
+     * Número del edificio en la calle
+     */
+    private int numEdificioCalle;
 }
