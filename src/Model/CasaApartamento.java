1912ae8bd5aaf7c6e6507d6b935ec47933413668
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 22:47:00 +0200

    agrego la variable distanciaCentro em Edificio y su java doc

--- src/Model/CasaApartamento.java
+++ src/Model/CasaApartamento.java
@@ -1,7 +1,11 @@
 package Model;
 
 /**
- * Clase de CasaApartamento, que hereda de Edificio y ofresen mas hospitalidad, por lo que da mascotas
+ * Clase de CasaApartamento, que hereda de Edificio y ofresen mas libertad, por lo que se mira la distancia al centro
  */
 public class CasaApartamento extends Edificio{
+    /**
+     * Distancia que tiene con el centro de la urbanizacion
+     */
+    private float distanciaCentro;
 }
