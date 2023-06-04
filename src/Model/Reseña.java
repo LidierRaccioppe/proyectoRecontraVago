b77caefb9274dabc3f9e5a4aa8399e6d0c5336da
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 22:18:13 +0200

    agrego la variable nombreUsuario en Reseña y su java doc

--- src/Model/Reseña.java
+++ src/Model/Reseña.java
@@ -4,4 +4,8 @@ package Model;
  * Clase de Reseña, que es hecha por un Usuario para dar su opinion de un Edificio/CasaApartamento/Hotel
  */
 public class Reseña {
+    /**
+     * nombre del usuario que hizo la reseña
+     */
+    private String nombreUsuario;
 }
