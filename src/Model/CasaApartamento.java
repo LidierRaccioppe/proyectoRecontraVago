a4b8b685d4ee7d09d0b183816b1d702b7cbe8d75
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 22:26:52 +0200

    hago que CasaApartamento herede de Edificio

--- src/Model/CasaApartamento.java
+++ src/Model/CasaApartamento.java
@@ -3,5 +3,5 @@ package Model;
 /**
  * Clase de CasaApartamento, que hereda de Edificio y ofresen mas hospitalidad, por lo que da mascotas
  */
-public class CasaApartamento {
+public class CasaApartamento extends Edificio{
 }
