e2f0cf4a877e0be45e13a174698abe49101a44bf
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-04 21:47:47 +0200

    agrego calidadServicios a Hotel y agrego su java doc

--- src/Model/Hotel.java
+++ src/Model/Hotel.java
@@ -5,4 +5,8 @@ public class Hotel extends Edificio{
      * Número de pisos que tiene el edificio
      */
     private int numPisos;
+    /**
+     * La calidad de los servicios que da el edificio
+     */
+    private float calidadServicios;
 }
