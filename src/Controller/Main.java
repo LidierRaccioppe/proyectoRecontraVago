4c4541ebcfae188056cba800ec0626c15403a887
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-06-05 10:00:54 +0200

    agrego el insertar usuario de modelo en Controller y su java doc

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,8 +1,12 @@
 package Controller;
 
+import Model.Modelo;
 import View.VistaVagos;
 
+import javax.swing.*;
+
 public class Main {
+        Modelo mod = new Modelo();
         public static void main(String[] args) {
                 // Este sera el proyecto de Lidier Máximo López Raccioppe.
                 // El proyecto sera hacer un programa parecido a trivago pero en java, destinado mas a la reseña.
@@ -12,9 +16,11 @@ public class Main {
 
 
         /**
-         * Para respetar el MVC y el observer
+         * Metodo para registrar un nuevo usuario en la base de datos
+         * @param nombreObt el campo de texto donde esta el nombre e identificador del Usuario
+         * @param contraseñaObt el campo de contraseña donde esta la contraseña del Usuario
          */
-        public void nuevoCoche(){
-
+        public void insertarUsuario(JTextField nombreObt, JPasswordField contraseñaObt){
+                mod.insertarUsuario(nombreObt,contraseñaObt);
         }
 }
